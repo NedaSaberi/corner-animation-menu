1b93caba453cb58f5ffce1dae4d7f8b369844d24
Neda Saberi <83552415+NedaSaberi@users.noreply.github.com>
2023-05-03 22:47:03 +0200

    Add files via upload

---
+++ p07 corner animation menu/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".menuBtn").click(function (e) {
+        e.preventDefault();
+      $(".circle-menu").toggleClass(' active');
+    });
+});
+
+// function test(event){
+//   console.log(event);
+// }
